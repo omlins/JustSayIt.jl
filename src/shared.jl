a01863961b05750cc8ccf5ded999ab3b8af16be8
Samuel Omlin <samuel.omlin@cscs.ch>
2022-03-16 19:29:57 +0100

    add Key and PkKey

--- src/shared.jl
+++ src/shared.jl
@@ -5,9 +5,11 @@ import MacroTools: splitdef, combinedef
 
 ## PYTHON MODULES
 const Vosk        = PyNULL()
-const Pynput      = PyNULL()
 const Sounddevice = PyNULL()
 const Zipfile     = PyNULL()
+const Pynput      = PyNULL()
+const Key         = PyNULL()
+
 
 function __init__()
     ENV["PYTHON"] = ""                                              # Force PyCall to use Conda.jl
@@ -18,9 +20,10 @@ function __init__()
         exit()
     end
     copy!(Vosk,        pyimport_pip("vosk"))
-    copy!(Pynput,      pyimport_pip("pynput"))
     copy!(Sounddevice, pyimport_pip("sounddevice"))
     copy!(Zipfile,     pyimport("zipfile"))
+    copy!(Pynput,      pyimport_pip("pynput"))
+    copy!(Key,         Pynput.keyboard.Key)
 end
 
 
@@ -46,6 +49,7 @@ const NOISES_ENGLISH = ["huh"]
 const DIGITS_ENGLISH = Dict("zero"=>"0", "one"=>"1", "two"=>"2", "three"=>"3", "four"=>"4", "five"=>"5", "six"=>"6", "seven"=>"7", "eight"=>"8", "nine"=>"9", "dot"=>".")
 const ALPHABET_ENGLISH = Dict("a"=>"a", "b"=>"b", "c"=>"c", "d"=>"d", "e"=>"e", "f"=>"f", "g"=>"g", "h"=>"h", "i"=>"i", "j"=>"j", "k"=>"k", "l"=>"l", "m"=>"m", "n"=>"n", "o"=>"o", "p"=>"p", "q"=>"q", "r"=>"r", "s"=>"s", "t"=>"t", "u"=>"u", "v"=>"v", "w"=>"w", "x"=>"x", "y"=>"y", "z"=>"z")
 const UNKNOWN_TOKEN = "[unk]"
+const PyKey = Union{Char, PyObject}
 
 @static if Sys.iswindows()
     const MODELDIR_PREFIX = joinpath(ENV["APPDATA"], "JustSayIt", "models")
@@ -97,3 +101,7 @@ function pretty_dict_string(dict::Dict{String,<:Any})
                 end...
                 ], "\n")
 end
+
+pretty_cmd_string(cmd::Union{Tuple,NTuple})   = join(map(pretty_cmd_string, cmd), " + ")
+pretty_cmd_string(cmd::PyObject)              = cmd.name
+pretty_cmd_string(cmd)                        = string(cmd)
