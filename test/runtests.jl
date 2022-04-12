79a43d9eed97bf31d7f947664c89a368780c374d
Samuel Omlin <samuel.omlin@cscs.ch>
2022-04-12 18:43:17 +0200

    add unit test runner

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,30 @@
-using JustSayIt
-using Test
+push!(LOAD_PATH, "../src")
 
-@testset "JustSayIt.jl" begin
-    # Write your tests here.
+import JustSayIt # Precompile it.
+
+excludedfiles = [ "test_excluded.jl"];
+
+function runtests()
+    exename   = joinpath(Sys.BINDIR, Base.julia_exename())
+    testdir   = pwd()
+    istest(f) = endswith(f, ".jl") && startswith(f, "test_")
+    testfiles = sort(filter(istest, readdir(testdir)))
+
+    nfail = 0
+    printstyled("Testing package JustSayIt.jl\n"; bold=true, color=:white)
+    for f in testfiles
+        println("")
+        if f ∈ excludedfiles
+            println("Test Skip:")
+            println("$f")
+            continue
+        end
+        try
+            run(`$exename -O3 --startup-file=no $(joinpath(testdir, f))`)
+        catch ex
+            nfail += 1
+        end
+    end
+    return nfail
 end
+exit(runtests())
