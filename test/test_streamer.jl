7d6c70a4586c310af9cd3752cf1754beab433511
Samuel Omlin <samuel.omlin@cscs.ch>
2022-05-30 19:21:28 +0200

    add audio sample based streamer unit tests

---
+++ test/test_streamer.jl
@@ -0,0 +1,36 @@
+using Test
+using JustSayIt
+using PyCall
+import JustSayIt: DEFAULT_MODEL_NAME, TYPE_MODEL_NAME, MODELDIR_PREFIX, DEFAULT_NOISES, DEFAULT_READER_ID, AUDIO_ELTYPE
+import JustSayIt: init_jsi, finalize_jsi, reader, start_reading, stop_reading, set_default_streamer, default_streamer
+
+# Test setup
+const SAMPLEDIR_CMD = joinpath("samples", "commands")
+
+commands = Dict("help"      => Help.help,
+                "type"      => Keyboard.type)
+modeldirs = Dict(DEFAULT_MODEL_NAME => joinpath(MODELDIR_PREFIX, "vosk-model-small-en-us-0.15"),
+                 TYPE_MODEL_NAME    => joinpath(MODELDIR_PREFIX, "vosk-model-small-en-us-0.15"))
+
+init_jsi(commands, modeldirs, DEFAULT_NOISES)
+start_reading(joinpath(SAMPLEDIR_CMD, "help.wav"))
+
+
+@testset "$(basename(@__FILE__))" begin
+    @testset "1. readbytes! (from wave reader)" begin
+        wav_reader = reader()
+        sample = zeros(UInt8, wav_reader.getnframes()*sizeof(AUDIO_ELTYPE))
+        bytes_read = readbytes!(wav_reader, sample)
+        @test bytes_read == 68938
+    end;
+    @testset "2. set default streamer" begin
+        @test_throws KeyError default_streamer()
+        set_default_streamer(reader, DEFAULT_READER_ID)
+        @test isa(default_streamer(), PyObject)
+    end
+end;
+
+
+# Test tear down
+stop_reading()
+finalize_jsi()
