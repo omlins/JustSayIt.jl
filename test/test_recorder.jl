5417ce530c16ddf409dd8f31dbb77b86454e1bca
Samuel Omlin <samuel.omlin@cscs.ch>
2022-05-30 19:48:29 +0200

    make mic recording testing OS dependent

--- test/test_recorder.jl
+++ test/test_recorder.jl
@@ -8,7 +8,7 @@ const SAMPLEDIR_CMD = joinpath("samples", "commands")
 
 
 @testset "$(basename(@__FILE__))" begin
-    @testset "1. start/stop/pause/restart recording from audio input cmd" begin  # NOTE: testing recording from mic fails in CI.
+    @testset "1. start/stop/pause/restart recording from audio input cmd" begin
         id = "help"
         cmd = `julia -e 'using JustSayIt; JustSayIt.read_wav(joinpath("samples","commands","help.wav"))'`
         @test isa(start_recording(;audio_input_cmd=cmd, id=id), Base.Process)
@@ -19,4 +19,16 @@ const SAMPLEDIR_CMD = joinpath("samples", "commands")
         @test isa(recorder(id), Base.Process)
         stop_recording(id=id)
     end;
+    @static if !Sys.iswindows()
+        @testset "2. start/stop/pause/restart recording from mic" begin
+            id = "mic"
+            @test isa(start_recording(id=id), PyObject)
+            @test active_recorder_id() == id
+            @test isa(recorder(id), PyObject)
+            pause_recording()
+            restart_recording()
+            @test isa(recorder(id), PyObject)
+            stop_recording(id=id)
+        end;
+    end
 end;
