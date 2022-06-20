11634b5746265762aca30204398e9b14298956e1
Samuel Omlin <samuel.omlin@cscs.ch>
2022-06-20 18:44:50 +0200

    remove unneeded uppercase_first implementation

--- src/Commands/Keyboard.jl
+++ src/Commands/Keyboard.jl
@@ -347,7 +347,7 @@ type
                 else           token_str = " " * token
                 end
             elseif (tokengroup_kind == letter_kind)
-                if (is_uppercase) token = uppercasefirst(token, active_lang) end
+                if (is_uppercase) token = uppercasefirst(token) end
                 token_str = token
             elseif (tokengroup_kind == digit_kind)
                 token_str = token
@@ -400,46 +400,9 @@ end
 
 
 function handle_uppercase(token::String, is_uppercase::Bool, active_lang::String)
-    if (is_uppercase) token = uppercasefirst(token, active_lang) end
+    if (is_uppercase) token = uppercasefirst(token) end
     if (active_lang == LANG.EN_US)
         if (startswith(token, "i'") || (token == "i")) token = uppercasefirst(token) end
-    end
-    return token
-end
-
-
-# This method extends Base.uppercasefirst to put the first letter to uppercase for cases that are not originally supported.
-function Base.uppercasefirst(token::String, active_lang::String)
-    token = uppercasefirst(token)
-    if (active_lang == LANG.ES)
-        if (token[1] == "á") token[1] = "Á"
-        elseif (token[1] == "é") token[1] = "É"
-        elseif (token[1] == "í") token[1] = "Í"
-        elseif (token[1] == "ñ") token[1] = "Ñ"
-        elseif (token[1] == "ó") token[1] = "Ó"
-        elseif (token[1] == "ê") token[1] = "Ê"
-        elseif (token[1] == "ú") token[1] = "Ú"
-        elseif (token[1] == "ü") token[1] = "Ü"
-        elseif (token[1] == "ý") token[1] = "Ý"
-        end
-    elseif (active_lang == LANG.FR)
-        if     (token[1] == "à") token[1] = "À"
-        elseif (token[1] == "â") token[1] = "Â"
-        elseif (token[1] == "ä") token[1] = "Ä"
-        elseif (token[1] == "æ") token[1] = "Æ"
-        elseif (token[1] == "ç") token[1] = "Ç"
-        elseif (token[1] == "è") token[1] = "È"
-        elseif (token[1] == "é") token[1] = "É"
-        elseif (token[1] == "ê") token[1] = "Ê"
-        elseif (token[1] == "ë") token[1] = "Ë"
-        elseif (token[1] == "î") token[1] = "Î"
-        elseif (token[1] == "ï") token[1] = "Ï"
-        elseif (token[1] == "ô") token[1] = "Ô"
-        elseif (token[1] == "œ") token[1] = "Œ"
-        elseif (token[1] == "ù") token[1] = "Ù"
-        elseif (token[1] == "û") token[1] = "Û"
-        elseif (token[1] == "ü") token[1] = "Ü"
-        end
     end
     return token
 end
