678c7ff0ee5b001398acca572afe8950bab98332
Samuel Omlin <samuel.omlin@cscs.ch>
2022-07-21 13:33:34 +0200

    add doc website

---
+++ docs/reflinks.jl
@@ -0,0 +1,12 @@
+reflinks = Dict(
+    "[Conda.jl]"                        => "https://github.com/JuliaPy/Conda.jl",
+    "[IJulia]"                          => "https://github.com/JuliaLang/IJulia.jl",
+    "[Julia]"                           => "https://julialang.org",
+    "[Julia REPL]"                      => "https://docs.julialang.org/en/v1/stdlib/REPL/",
+    "[MacroTools.jl]"                   => "https://github.com/FluxML/MacroTools.jl",
+    "[PyCall.jl]"                       => "https://github.com/JuliaPy/PyCall.jl",
+    "[pynput]"                          => "https://github.com/moses-palmer/pynput",
+    "[sounddevice]"                     => "https://github.com/spatialaudio/python-sounddevice",
+    "[vosk]"                            => "https://github.com/alphacep/vosk-api",
+    "[Vosk Speech Recognition Toolkit]" => "https://alphacephei.com/vosk/",
+)
