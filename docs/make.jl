bf1cebea57456c2c1521d5460412121a9dd5c065
Samuel Omlin <samuel.omlin@cscs.ch>
2022-07-21 13:33:16 +0200

    add doc website

---
+++ docs/make.jl
@@ -0,0 +1,49 @@
+using JustSayIt
+using Documenter
+using DocExtensions
+using DocExtensions.DocumenterExtensions
+
+const DOCSRC      = joinpath(@__DIR__, "src")
+const EXAMPLEROOT = joinpath(@__DIR__, "..", "config_examples")
+
+DocMeta.setdocmeta!(JustSayIt, :DocTestSetup, :(using JustSayIt); recursive=true)
+
+
+@info "Preprocessing .MD-files..."
+include("reflinks.jl")
+MarkdownExtensions.expand_reflinks(reflinks; rootdir=DOCSRC)
+
+
+@info "Building documentation website using Documenter.jl..."
+makedocs(;
+    modules  = [JustSayIt],
+    authors  = "Samuel Omlin and contributors",
+    repo     = "https://github.com/omlins/JustSayIt.jl/blob/{commit}{path}#{line}",
+    sitename = "JustSayIt.jl",
+    format   = Documenter.HTML(;
+        prettyurls       = get(ENV, "CI", "false") == "true",
+        canonical        = "https://omlins.github.io/JustSayIt.jl",
+        collapselevel    = 1,
+        sidebar_sitename = true,
+        edit_link        = "main",
+        #assets           = [asset("https://img.shields.io/github/stars/omlins/JustSayIt.jl.svg", class = :ico)],
+        #warn_outdated    = true,
+    ),
+    pages   = [
+        "Introduction and Quick Start"  => "index.md",
+        "Usage"                         => "usage.md",
+        "Examples"                      => [hide("..." => "examples.md"),
+                                            "examples/config_custom_function.md",
+                                           ],
+        "Software reference"            => "software.md",
+        "High-level API reference"      => "api.md",
+    ],
+)
+
+
+@info "Deploying docs..."
+deploydocs(;
+    repo         = "github.com/omlins/JustSayIt.jl",
+    push_preview = true,
+    devbranch    = "main",
+)
