6aaaba3daeaebe70480d391ad17ae28f36d93ad0
Samuel Omlin <samuel.omlin@cscs.ch>
2022-07-21 17:13:18 +0200

    Add JSI reflink

--- docs/reflinks.jl
+++ docs/reflinks.jl
@@ -3,6 +3,7 @@ reflinks = Dict(
     "[IJulia]"                          => "https://github.com/JuliaLang/IJulia.jl",
     "[Julia]"                           => "https://julialang.org",
     "[Julia REPL]"                      => "https://docs.julialang.org/en/v1/stdlib/REPL/",
+    "[JustSayIt.jl]"                    => "https://github.com/omlins/JustSayIt.jl",
     "[MacroTools.jl]"                   => "https://github.com/FluxML/MacroTools.jl",
     "[PyCall.jl]"                       => "https://github.com/JuliaPy/PyCall.jl",
     "[pynput]"                          => "https://github.com/moses-palmer/pynput",
