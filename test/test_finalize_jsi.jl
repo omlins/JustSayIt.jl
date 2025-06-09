a7c5090be18bbc44c1c44d3bfee2f3c4d8a1285d
Samuel Omlin <samuel.omlin@cscs.ch>
2025-06-09 17:19:53 +0200

    update finalize tests

--- test/test_finalize_jsi.jl
+++ test/test_finalize_jsi.jl
@@ -2,13 +2,13 @@ using Test
 using JustSayIt
 using JustSayIt.API
 using PyCall
-import JustSayIt: MODELNAME, MODELDIR_PREFIX, DEFAULT_NOISES
+import JustSayIt: MODELNAME, VOSK_MODELDIR_PREFIX, DEFAULT_NOISES
 import JustSayIt: init_jsi, finalize_jsi
 
 commands = Dict("help"      => Help.help,
                 "type"      => Keyboard.type)
-modeldirs = Dict(MODELNAME.DEFAULT.EN_US => joinpath(MODELDIR_PREFIX, "vosk-model-small-en-us-0.15"),
-                 MODELNAME.TYPE.EN_US    => joinpath(MODELDIR_PREFIX, "vosk-model-small-en-us-0.15"))
+modeldirs = Dict(MODELNAME.DEFAULT.EN_US => joinpath(VOSK_MODELDIR_PREFIX, "vosk-model-small-en-us-0.15"),
+                 MODELNAME.TYPE.EN_US    => joinpath(VOSK_MODELDIR_PREFIX, "vosk-model-small-en-us-0.15"))
 
 init_jsi(commands, modeldirs, DEFAULT_NOISES)
 
