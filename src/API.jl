1721aa5f072fd9461dade9ee32e7ddb154e59c15
Samuel Omlin <samuel.omlin@cscs.ch>
2025-06-09 17:00:48 +0200

    export say

--- src/API.jl
+++ src/API.jl
@@ -5,6 +5,7 @@ Application Programming Interface (API) of JustSayIt.
 
 #### Macros
 - [`@voiceargs`](@ref)
+- [`@voiceconfig`](@ref)
 
 #### Functions
 - [`is_next`](@ref)
@@ -19,6 +20,6 @@ Application Programming Interface (API) of JustSayIt.
 To see a description of a function, macro or module type `?<functionname>`, `?<macroname>` (including the `@`) or `?<modulename>`, respectively.
 """
 module API
-    import ..JustSayIt: @voiceargs, is_next, are_next, pause_recording, restart_recording, LANG, MODELNAME
-    export @voiceargs, is_next, are_next, pause_recording, restart_recording, LANG, MODELNAME
+    import ..JustSayIt: @voiceargs, @voiceconfig, is_next, are_next, pause_recording, restart_recording, LANG, MODELNAME, say
+    export @voiceargs, @voiceconfig, is_next, are_next, pause_recording, restart_recording, LANG, MODELNAME, say
 end
