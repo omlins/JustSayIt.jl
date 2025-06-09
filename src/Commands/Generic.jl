e3f6006d17ee363221d9d4b53f15dd17e068d214
Samuel Omlin <samuel.omlin@cscs.ch>
2025-06-09 17:01:59 +0200

    update Generic

--- src/Commands/Generic.jl
+++ src/Commands/Generic.jl
@@ -13,15 +13,15 @@ To see a description of a function type `?<functionname>`.
 module Generic
 
 using ..Exceptions
-import ..JustSayIt: @voiceargs, default_language, next_token, execute, LANG, COUNTS, UNKNOWN_TOKEN
+import ..JustSayIt: @voiceargs, default_language, next_token, execute, LANG, COUNTS, COUNTS_MAPPING, UNKNOWN_TOKEN
+export execute_n_times
+
+## HELPER FUNCTIONS
+
+interpret_count(input::AbstractString) = (return COUNTS_MAPPING[default_language()][input])
 
 
-## CONSTANTS
-
-
-## FUNCTIONS
-
-interpret_count(input::AbstractString) = (return COUNTS[default_language()][input])
+## API FUNCTIONS
 
 @doc """
     execute_n_times `n` `(complement of) cmd`
@@ -29,7 +29,7 @@ interpret_count(input::AbstractString) = (return COUNTS[default_language()][inpu
 Execute a command n times.
 """
 execute_n_times
-@voiceargs n=>(valid_input=(LANG.DE=>[keys(COUNTS[LANG.DE])...], LANG.EN_US=>[keys(COUNTS[LANG.EN_US])...], LANG.ES=>[keys(COUNTS[LANG.ES])...], LANG.FR=>[keys(COUNTS[LANG.FR])...]), interpret_function=interpret_count, use_max_speed=true) function execute_n_times(n::Int, commands::Dict{String,<:Any}; cmdname::String="do")
+@voiceargs n=>(valid_input=COUNTS, interpreter=interpret_count, use_max_speed=true) function execute_n_times(n::Int, commands::Dict{String,<:Any}; cmdname::String="do")
     # count = parse(Int, token) # NOTE: this is safe as the count is always an integer.
     cmd_complement = next_token([keys(commands)...]; ignore_unknown=false)
     if (cmd_complement == UNKNOWN_TOKEN) @InsecureRecognitionException("unknown command.") end
