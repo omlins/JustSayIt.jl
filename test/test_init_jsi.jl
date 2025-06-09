106203ea9a9b28d2fab9c6ba19cf6179f41e8292
Samuel Omlin <samuel.omlin@cscs.ch>
2025-06-09 17:20:30 +0200

    update init tests

--- test/test_init_jsi.jl
+++ test/test_init_jsi.jl
@@ -2,7 +2,7 @@ using Test
 using JustSayIt
 using JustSayIt.API
 using PyCall
-import JustSayIt: MODELNAME, MODELDIR_PREFIX, COMMAND_RECOGNIZER_ID
+import JustSayIt: MODELNAME, VOSK_MODELDIR_PREFIX, COMMAND_RECOGNIZER_ID
 import JustSayIt: init_jsi, finalize_jsi, command_names, command, noises_names, noises, model, recognizer, Recognizer
 
 @testset "$(basename(@__FILE__))" begin
@@ -11,8 +11,8 @@ import JustSayIt: init_jsi, finalize_jsi, command_names, command, noises_names, 
                         "type"    => Keyboard.type,
                         "redo"    => (Key.ctrl, Key.shift, 'z'),
                         "upwards" => Key.page_up)
-        modeldirs = Dict(MODELNAME.DEFAULT.EN_US => joinpath(MODELDIR_PREFIX, "vosk-model-small-en-us-0.15"),
-                         MODELNAME.TYPE.EN_US    => joinpath(MODELDIR_PREFIX, "vosk-model-small-en-us-0.15"))
+        modeldirs = Dict(MODELNAME.DEFAULT.EN_US => joinpath(VOSK_MODELDIR_PREFIX, "vosk-model-small-en-us-0.15"),
+                         MODELNAME.TYPE.EN_US    => joinpath(VOSK_MODELDIR_PREFIX, "vosk-model-small-en-us-0.15"))
         mynoises  = Dict(MODELNAME.DEFAULT.EN_US => ["huh"],
                          MODELNAME.TYPE.EN_US    => ["huhuh"])
         init_jsi(commands, modeldirs, mynoises)
