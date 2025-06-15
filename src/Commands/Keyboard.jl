90298d75600a02895e5002e2c97fcd5dc808bb6f
Samuel Omlin <samuel.omlin@cscs.ch>
2025-06-15 12:21:14 +0200

    refactor Commands

--- src/Commands/Keyboard.jl
+++ src/Commands/Keyboard.jl
@@ -5,7 +5,33 @@ Provides functions for controlling the keyboard by voice.
 
 # Functions
 
-###### Typing
+###### Text typing
+- [`Keyboard.type_sentence`](@ref)
+- [`Keyboard.type_sentence_lowercase`](@ref)
+
+###### Word typing
+- [`Keyboard.type_first_lowercase`](@ref)
+- [`Keyboard.type_first_uppercase`](@ref)
+- [`Keyboard.type_lowercase`](@ref)
+- [`Keyboard.type_uppercase`](@ref)
+- [`Keyboard.type_flatcase`](@ref)
+- [`Keyboard.type_snakecase`](@ref)
+- [`Keyboard.type_camelcase`](@ref)
+- [`Keyboard.type_constantcase`](@ref)
+
+###### Letter and digit typing
+- [`Keyboard.type_letter`](@ref)
+- [`Keyboard.type_majuscule`](@ref)
+- [`Keyboard.type_letters`](@ref)
+- [`Keyboard.type_capitals`](@ref)
+- [`Keyboard.type_digits`](@ref)
+
+###### Key pressing
+- [`Keyboard.press_keys`](@ref)
+- [`Keyboard.press_delete`](@ref)
+- [`Keyboard.press_backspace`](@ref)
+
+###### String typing
 - [`Keyboard.type`](@ref)
 
 To see a description of a function type `?<functionname>`.
@@ -14,25 +40,10 @@ See also: [`Mouse`](@ref)
 """
 module Keyboard
 
+using ..JustSayIt.API
+import ..JustSayIt: Key, PyKey
 using PyCall
-using ..Exceptions
-using ..STT: next_wordgroup, next_letter, next_letters, next_digit, next_digits, handle_uppercase, handle_first_lower!, remove_end_punctuation
-import ..JustSayIt: @voiceargs, @voiceconfig, pyimport_pip, controller, set_controller, PyKey, default_language, type_languages, lang_str, LANG, LANGUAGES, LANGUAGES_MAPPING, LANG_STR, ALPHABET, DIGITS, DIGITS_MAPPING, MODELTYPE_DEFAULT, MODELNAME, modelname, tic, toc, is_next, are_next, next_token, next_tokengroup, all_consumed, was_partial_recognition, InsecureRecognitionException, reset_all, do_delayed_resets, interpret_enum, UNKNOWN_TOKEN, MODELTYPE_SPEECH, MODELTYPE_DEFAULT
-export press_keys, press_delete, press_backspace, type_sentence, type_sentence_lowercase, type_first_lowercase, type_first_uppercase, type_lowercase, type_uppercase, type_flatcase, type_snakecase, type_camelcase, type_constantcase, type_letter, type_majuscule, type_letters, type_capitals, type_digits
-
-## PYTHON MODULES
-
-const Pynput    = PyNULL()
-const Tkinter   = PyNULL()
-
-function __init__()
-    if !haskey(ENV, "JSI_USE_PYTHON") ENV["JSI_USE_PYTHON"] = "1" end
-    if ENV["JSI_USE_PYTHON"] == "1"
-        copy!(Pynput, pyimport_pip("pynput"))
-        copy!(Tkinter, pyimport_pip("tkinter"))
-        set_controller("keyboard", Pynput.keyboard.Controller())
-    end
-end
+public press_keys, press_delete, press_backspace, type_sentence, type_sentence_lowercase, type_first_lowercase, type_first_uppercase, type_lowercase, type_uppercase, type_flatcase, type_snakecase, type_camelcase, type_constantcase, type_letter, type_majuscule, type_letters, type_capitals, type_digits
 
 
 ## HELPER FUNCTIONS
@@ -46,7 +57,7 @@ function type_string(str::AbstractString; do_keystrokes::Bool=true, press_durati
         end
         keys = map(convert_key, [str...])
         for key in keys
-            if (!single_line && key == "\n") key = Pynput.keyboard.Key.enter end
+            if (!single_line && key == "\n") key = Key.enter end
             keyboard.press(key)
             sleep(press_duration)
             keyboard.release(key)
@@ -55,6 +66,20 @@ function type_string(str::AbstractString; do_keystrokes::Bool=true, press_durati
     end
 end
 
+function handle_uppercase(token::String, is_uppercase::Bool, active_lang::String)
+    if (is_uppercase) token = uppercasefirst(token) end
+    if (active_lang == LANG.EN_US)
+        if (startswith(token, "i'") || (token == "i")) token = uppercasefirst(token) end
+    end
+    return token
+end
+
+function handle_first_lower!(words::AbstractArray{<:AbstractString}, active_lang::String)
+    words[begin] = handle_uppercase(lowercase(words[begin]), false, active_lang)
+    return words
+end
+
+remove_end_punctuation(words_str::AbstractString, lang::String) = (lang != LANG_AUTO) ? replace(words_str, r"\p{P}+$" => "") : words_str
 
 @voiceargs words=>(modeltype=MODELTYPE_DEFAULT, ignore_unknown=true) _next_short_wordgroup(words::String...) = return collect(words)
 
@@ -71,14 +96,14 @@ function type_fixedcase(case::Case; lang::String=default_language())
     elseif (case == camel      ) words_str = join(uppercasefirst.(words))
     elseif (case == constant   ) words_str = join(uppercase.(words), "_")
     end
-    type_string(remove_end_punctuation(words_str))
+    type_string(remove_end_punctuation(words_str, lang))
 end
 
 convert_key(key::Char)     = string(key)
 convert_key(key::PyObject) = key
 
 
-## API FUNCTIONS
+## COMMAND FUNCTIONS
 
 # (Press keys)
 
@@ -103,19 +128,19 @@ let
 end
 
 "Press delete key."
-press_delete(; count::Integer=1) = press_keys(Pynput.keyboard.Key.delete; count=count)
+press_delete(; count::Integer=1) = press_keys(Key.delete; count=count)
 
 "Press backspace key."
-press_backspace(; count::Integer=1) = press_keys(Pynput.keyboard.Key.backspace; count=count)
+press_backspace(; count::Integer=1) = press_keys(Key.backspace; count=count)
 
 
 # (Type words)
 
 "Type a sentence (starting uppercase)."
-type_sentence(; lang::String=default_language()) = type_string(remove_end_punctuation(uppercasefirst(join(next_wordgroup(lang=lang), " "))))
+type_sentence(; lang::String=default_language()) = type_string(remove_end_punctuation(uppercasefirst(join(next_wordgroup(lang=lang), " ")), lang))
 
 "Type a sentence (starting lowercase)."
-type_sentence_lowercase(; lang::String=default_language()) = type_string(remove_end_punctuation(join(handle_first_lower!(next_wordgroup(lang=lang), lang), " ")))
+type_sentence_lowercase(; lang::String=default_language()) = type_string(remove_end_punctuation(join(handle_first_lower!(next_wordgroup(lang=lang), lang), " "), lang))
 
 "Type words, starting with the first in lowercase (separated with space)."
 type_first_lowercase(; lang::String=default_language()) = type_fixedcase(first_lower; lang=lang)
@@ -160,44 +185,13 @@ type_capitals(; lang::String=default_language()) = type_string(join(uppercase.(n
 type_digits(; lang::String=default_language()) = type_string(join((next_digits(lang=lang)), ""))
 
 
-function get_selection_content()
-    sleep(0.1)
-    root = Tkinter.Tk() # NOTE: it seems to be necessary that the root object is created after the keyboard copy shortcut is executed has otherwise the clipboard does sometimes not contain the new content.
-    root.withdraw()
-    root.update()
-    content = ""
-    try
-        content = root.selection_get(selection="PRIMARY")
-    catch e
-        if isa(e, PyCall.PyError)
-            content = ""
-        else
-            rethrow(e)
-        end
-    end
-    root.update()
-    root.destroy()
-    return content
-end
+# (Type text)
 
+"""
+    type(text::AbstractString)
 
-function get_clipboard_content()
-    root = Tkinter.Tk() # NOTE: it seems to be necessary that the root object is created after the keyboard copy shortcut is executed has otherwise the clipboard does sometimes not contain the new content.
-    root.withdraw()
-    root.update()
-    content = ""
-    try
-        content = root.clipboard_get()
-    catch e
-        if isa(e, PyCall.PyError)
-            content = ""
-        else
-            rethrow(e)
-        end
-    end
-    root.update()
-    root.destroy()
-    return content
-end
+Type `text` with the keyboard.
+"""
+type(text::AbstractString) = type_string(text)
 
 end # module Keyboard
