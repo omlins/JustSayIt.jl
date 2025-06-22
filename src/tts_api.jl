ca2a8a1089f76b5ade5eb40cc3cd7493d77f52dc
Samuel Omlin <samuel.omlin@cscs.ch>
2025-06-22 11:59:05 +0200

    fix tts progress

--- src/tts_api.jl
+++ src/tts_api.jl
@@ -3,7 +3,7 @@
 is_playing_tts(; enginename::String=tts(), streamname::String=TTS_DEFAULT_STREAM) = is_tts_stream(enginename, streamname) && stream(enginename, streamname).is_playing()
 pause_tts(; enginename::String=tts(), streamname::String=TTS_DEFAULT_STREAM)      = if is_playing_tts(enginename=enginename, streamname=streamname) stream(enginename, streamname).pause() end
 resume_tts(; enginename::String=tts(), streamname::String=TTS_DEFAULT_STREAM)     = if is_playing_tts(enginename=enginename, streamname=streamname) stream(enginename, streamname).resume() end
-stop_tts(; enginename::String=tts(), streamname::String=TTS_DEFAULT_STREAM)       = if is_playing_tts(enginename=enginename, streamname=streamname) stream(enginename, streamname).stop() end
+stop_tts(; enginename::String=tts(), streamname::String=TTS_DEFAULT_STREAM)       = if is_playing_tts(enginename=enginename, streamname=streamname) stream(enginename, streamname).stop(); stop_progresser(enginename=enginename, streamname=streamname) end
 
 
 ## API MACROS
