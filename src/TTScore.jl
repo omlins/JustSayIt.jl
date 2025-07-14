6dacb455a8d2672fc528aae33d71053f8a198640
Samuel Omlin <samuel.omlin@cscs.ch>
2025-07-14 20:13:31 +0200

    add imports and exports to TTScore

--- src/TTScore.jl
+++ src/TTScore.jl
@@ -2,12 +2,15 @@ module TTScore
 
 ## Imports
 using PyCall, Preferences
+using ..JustSayIt
 import Base.Threads
 import ..JustSayIt: tic, toc, use_gpu, use_tts
 using ..JustSayIt.Exceptions
 import ..JustSayIt: Sounddevice
 using ..JustSayIt: RealtimeTTS
 using ..JustSayIt: Time
+using PromptingTools
+const PT = PromptingTools
 
 ## Include of global constants and macros
 include("tts_constants.jl")
@@ -18,6 +21,6 @@ include("tts_api.jl")
 
 ## Exports (need to be after include of submodules if re-exports from them)
 export init_tts, finalize_tts
-export say, @voiceinfo, set_tts_async_default, tts_async_default
+export say, @voiceinfo, set_tts_async_default, tts_async_default, tts, dump_audio
 
 end # module JustSayIt
